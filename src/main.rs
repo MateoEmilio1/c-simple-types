b402743d838b3659066587582d562f7fd534ae8d
Tute <mateoyastor60@gmail.com>
2022-05-24 00:02:07 -0300

    Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+#![allow(dead_code, unused_variables)]
+
 fn main() {
-    println!("Hello, world!");
+    let coords: (f32, f32) = (6.3, 15.0);
+
+    print_difference( coords.0 , coords.1);
+
+    let coords_arr: [f32; 2] = [coords.0 , coords.1];
+
+    print_array(coords_arr);
+
+    let series = [1, 1, 2, 3, 5, 8, 13];
+
+    ding(series[6]);
+
+    let mess = ([3, 2], 3.14, [(false, -3), (true, -100)], 5, "candy");
+
+}
+
+fn print_difference(x: f32, y: f32) {
+    println!("Difference between {} and {} is {}", x, y, (x - y).abs());
+}
+
+fn print_array(a: [f32; 2]) {
+    println!("The coordinates are ({}, {})", a[0], a[1]);
+}
+
+fn ding(x: i32) {
+    if x == 13 {
+        println!("Ding, you found 13!");
+    }
+}
+
+fn on_off(val: bool) {
+    if val {
+        println!("Lights are on!");
+    }
+}
+
+fn print_distance(z: (f32, f32)) {
+    
+    println!(
+        "Distance to the origin is {}",
+        ( z.0.powf(2.0) + z.1.powf(2.0) ).sqrt());
 }
